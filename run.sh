d0ca3843e8d64836380de238f4aa7204b51cf049
Ian Maquignaz <ian.maquignaz.1@ulaval.ca>
2024-01-30 14:47:21 -0500

    works!

--- run.sh
+++ run.sh
@@ -5,13 +5,23 @@
 # conda env create -f environment.yml
 # conda init bash
 
-CKPTS_DIR="text2light_released_model"
+CKPTS_DIR="./text2light_released_model"
 
+# python3 text2light.py \
+#     -rg $CKPTS_DIR/global_sampler_clip \
+#     -rl $CKPTS_DIR/local_sampler_outdoor \
+#     --outdir ./generated_panorama \
+#     --text "A storm is approaching on the horizon" \
+#     --clip clip_emb.npy \
+#     --sritmo $CKPTS_DIR/sritmo.pth \
+#     --sr_factor 4
+
+
+# Skip HDR
 python3 text2light.py \
     -rg $CKPTS_DIR/global_sampler_clip \
     -rl $CKPTS_DIR/local_sampler_outdoor \
     --outdir ./generated_panorama \
-    --text "YOUR SCENE DESCRIPTION" \
+    --text "A storm is approaching on the horizon" \
     --clip clip_emb.npy \
-    --sritmo $CKPTS_DIR/sritmo.pth \
     --sr_factor 4
