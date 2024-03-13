93ce62d947b11167c66131950c2f7e9fa1835799
Ian Maquignaz <ian.maquignaz.1@ulaval.ca>
2024-03-13 17:26:48 -0400

    Update to run.sh and added prompt file

--- run.sh
+++ run.sh
@@ -7,21 +7,34 @@
 
 CKPTS_DIR="./text2light_released_model"
 
+# HDR
+echo "Have you called \"conda activate text2light\"?"
+
+while IFS= read -r line
+do
+  # Replace 'your_command' with your actual command
+  echo -e "\nPromt << $line"
+  python3 text2light.py \
+    -rg $CKPTS_DIR/global_sampler_clip \
+    -rl $CKPTS_DIR/local_sampler_outdoor \
+    --outdir ./generated_panorama \
+    --text "$line" \
+    --clip clip_emb.npy \
+    --sritmo $CKPTS_DIR/sritmo.pth \
+    --sr_factor 1
+done < "alt_outdoor.txt"
+exit 0
+
+# Other Parameters
+# --sr_factor 4 # scale factor for super resolution
+# --text "photorealistc baseball diamond" \
+
+
+# Skip HDR
 # python3 text2light.py \
 #     -rg $CKPTS_DIR/global_sampler_clip \
 #     -rl $CKPTS_DIR/local_sampler_outdoor \
 #     --outdir ./generated_panorama \
-#     --text "A storm is approaching on the horizon" \
+#     --text "photorealistc baseball diamond" \
 #     --clip clip_emb.npy \
-#     --sritmo $CKPTS_DIR/sritmo.pth \
 #     --sr_factor 4
-
-
-# Skip HDR
-python3 text2light.py \
-    -rg $CKPTS_DIR/global_sampler_clip \
-    -rl $CKPTS_DIR/local_sampler_outdoor \
-    --outdir ./generated_panorama \
-    --text "A storm is approaching on the horizon" \
-    --clip clip_emb.npy \
-    --sr_factor 4
